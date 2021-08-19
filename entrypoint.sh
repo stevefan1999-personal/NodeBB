fa56de1ce6bbc680c45c98af91732d1629d2b7e4
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2021-08-20 01:29:31 +0800

    Create entrypoint.sh
    from https://github.com/rez0n/docker-nodebb

---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+if ! [ -e /data/config.json ]; then
+
+    if [ -n "$DATABASE" ]; then
+
+        if [ "$DATABASE" == "redis" ]; then
+            url=$URL database=$DATABASE redis__database=$DB_NAME redis__password=$DB_PASSWORD redis__host=$DB_HOST redis__port=$DB_PORT node app --setup --series
+        elif [ "$DATABASE" == "mongo" ]; then
+            url=$URL database=$DATABASE mongo__username=$DB_USER mongo__password=$DB_PASSWORD mongo__host=$DB_HOST mongo__port=$DB_PORT node app --setup --series
+        fi
+
+    else
+        echo "Database setting is invalid"
+    fi
+
+    mv config.json /data/config.json \
+    && ln -s /data/config.json /usr/src/app/config.json
+else
+    # Link config file (in cases when you recreated container)
+    if [ ! -e /usr/src/app/config.json ]; then
+        ln -s /data/config.json /usr/src/app/config.json
+    fi
+
+fi
+
+if [ ! -e /data/uploads ]; then
+    mv /usr/src/app/public/uploads /data/uploads \
+    && ln -s /data/uploads /usr/src/app/public/uploads
+else
+    rm -rf /usr/src/app/public/uploads \
+    && ln -s /data/uploads /usr/src/app/public/uploads
+fi
+
+if [ ! -e /data/package.json ]; then
+    mv /usr/src/app/package.json /data/package.json \
+    && ln -s /data/package.json /usr/src/app/package.json
+else
+    rm /usr/src/app/package.json \
+    && ln -s /data/package.json /usr/src/app/package.json
+fi
+
+if [ -f config.json ]; then
+    /usr/src/app/nodebb build --series
+    /usr/src/app/nodebb upgrade -mips
+fi
+
+exec "$@"
