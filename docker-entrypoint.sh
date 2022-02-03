1e1065a3ef38741553206628c951f381211046c9
Steve Fan <19037626d@connect.polyu.hk>
2022-02-03 14:24:20 +0800

    specify the config file location instead of creating it

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 export CONFIG_DIR=/opt/config
+export CONFIG=$CONFIG_DIR/config.json
 
 mkdir -p $CONFIG_DIR
 chmod 777 -R $CONFIG_DIR
 
-[[ -f $CONFIG_DIR/config.json ]] || touch $CONFIG_DIR/config.json
 [[ -f $CONFIG_DIR/package.json ]] || cp install/package.json $CONFIG_DIR/package.json
+[[ -f $CONFIG_DIR/package-lock.json ]] || echo {} > $CONFIG_DIR/package-lock.json
 
 ln -s $CONFIG_DIR/package.json package.json
-ln -s $CONFIG_DIR/config.json config.json
+ln -s $CONFIG_DIR/package-lock.json package-lock.json
 
 npm install --only=prod
-npm cache clean --force
 
 ./nodebb build
 ./nodebb start
